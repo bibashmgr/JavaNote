d3e0ae29f6d1857774cd71fdc2f76b6e76bed34f
bibashmgr <bibashthapamagar21>
2020-04-30 18:52:32 +0545

    Adding some changes

---
+++ ControlStatementsInJava/ForLoopInJava/ForStatement.java
@@ -0,0 +1,15 @@
+package JavaNote.ControlStatementsInJava.ForLoopInJava;
+
+public class ForStatement {
+
+    public static void main(String[] args) {
+        
+        for(int i = 1; i< 10 ;i=i+2){
+
+            System.out.println("Odd numbers from 1 to 10:");
+            System.out.println(i);
+
+        }
+    }
+
+}
---
+++ ControlStatementsInJava/ForLoopInJava/readme.md
@@ -0,0 +1,11 @@
+> # For Loop In Java:
+The for loop is used to iterate a part of the program several times.
+
+__Syntax:__
+```java
+for( initialization ;condition ; incremnet/decrement ){ 
+
+//statement or code to be executed 
+
+} 
+```
